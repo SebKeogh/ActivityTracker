6860935941a32748530e2ddad5bf545a9bf7f856
Sebastian Keogh <s.keogh@reeltimegaming.com>
2025-01-06 17:47:09 +1000

    V2 of the App Layout. Looks scuffed as hell but it's the basic idea I want to stick with.

--- app/src/main/java/com/example/activitytracker/MainActivity.kt
+++ app/src/main/java/com/example/activitytracker/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.activitytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,8 +19,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -32,28 +38,39 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BestLayout() {
     val domains = DomainData.domains
+    // TODO:: This should probably be a lazy column, for scrolling
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         domains.forEach { domain ->
-            Row(modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp), horizontalArrangement = Arrangement.Center)
-            {
-                Button(
-                    onClick = { /* Handle domain click */ },
+            Box( modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .background(Color.DarkGray)
+                .border(width = 1.dp, color = Color.Blue)
+            ) {
+                Column(
+                    modifier = Modifier.align(Alignment.TopCenter).padding(6.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = domain.name, style = MaterialTheme.typography.labelLarge)
-                        Text(text = "00h 00m 00s", style = MaterialTheme.typography.labelSmall)
-                    }
-                }
-            }
-            Row(modifier = Modifier.fillMaxWidth().padding(bottom = 24.dp), horizontalArrangement = Arrangement.SpaceEvenly)
-            {
-                domain.categories.forEach { category ->
-                    Button(
-                        onClick = { /* Handle category clicking */ },
+                    Text(text = domain.name, style = MaterialTheme.typography.labelLarge)
+                    Text(text = "00:00:00", style = MaterialTheme.typography.labelSmall)
+                    Row (modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
-                        Text(text = category.name, style = MaterialTheme.typography.labelMedium)
+                        domain.categories.forEach { category ->
+                            Button(
+                                onClick = { /* TODO:: This */ },
+                                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp)
+                            ) {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Text(text = category.name)
+                                    Text(text = "00:00:00", style = MaterialTheme.typography.labelSmall, fontSize = 8.sp)
+                                }
+                            }
+                        }
                     }
                 }
             }
